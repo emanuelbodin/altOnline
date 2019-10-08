f935a8c681284a01adc1bbe5f4198d5e9db4af3b
Emanuel <bodinemanuel@gmail.com>
2019-10-08 16:09:09 +0200

    h

--- tables.py
+++ tables.py
@@ -17,6 +17,6 @@ def tablesInit():
     mycursor.execute("CREATE TABLE IF NOT EXISTS users (email varchar(255), id_no int(11), password varchar(255), name varchar(255), street varchar(255), phone varchar(255), newsletter bit, city varchar(255), PRIMARY KEY (email), FOREIGN KEY (city) REFERENCES city_zip_code(city))")
     mycursor.execute("CREATE TABLE IF NOT EXISTS orders (order_id int(11), tracking_id varchar(255), order_date datetime, order_status varchar(255), last_change_date datetime, payment_ref varchar(255),  user_email varchar(255))")
     mycursor.execute("CREATE TABLE IF NOT EXISTS order_items (product_title varchar(255), orderID int(11), quantity int(11), current_prodyct_price int(11), current_product_discount int(11), current_product_tax int(11), CONSTRAINT PK_ORDERITEMS PRIMARY KEY (product_title, orderID), FOREIGN KEY (product_title) REFERENCES products(title), FOREIGN KEY (orderID) REFERENCES orders(order_id))")
-    #mycursor.execute("CREATE TABLE IF NOT EXISTS reviews (product_title varchar(255), email varchar(255), score int(11), review_text text(100), CONSTRAINT PK_REVIEWS PRIMARY KEY (product_title, email), FOREIGN KEY (product_title) REFERENCES products(title))")
+    mycursor.execute("CREATE TABLE IF NOT EXISTS reviews (product_title varchar(255), email varchar(255), score int(11), review_text text(100), CONSTRAINT PK_REVIEWS PRIMARY KEY (product_title, email), FOREIGN KEY (product_title) REFERENCES products(title))")
 
 tablesInit()
